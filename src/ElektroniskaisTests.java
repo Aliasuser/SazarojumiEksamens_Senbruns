98fc9a4f4ce625a05b204de356fd5cb758adb387
AliasXIX <arnis.senbruns@sk.lvt.lv>
2025-06-07 13:33:36 +0300

    Izveidots jautajumu lauks, atbildes pogas ar grupu, atbildes logs un parbaudes poga.

--- src/ElektroniskaisTests.java
+++ src/ElektroniskaisTests.java
@@ -1,4 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
 
 public class ElektroniskaisTests extends JFrame{
 	/**
@@ -6,16 +16,37 @@ public class ElektroniskaisTests extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	JLabel jautajumuLauks;
+	JRadioButton[] atbilzuPogas;
+	JButton parbauditPoga;
+	ButtonGroup poguGrupa;
+	JPanel atbilzuLogs;
+	
 	public ElektroniskaisTests() {
-		setSize(500, 500);
+		setSize(800, 450);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setTitle("Java Elektroniskais Tests");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout(10,10));
+		
+		jautajumuLauks = new JLabel("Tests");
+		jautajumuLauks.setFont(new Font("Monospaced", Font.BOLD, 16));
+		jautajumuLauks.setHorizontalAlignment(SwingConstants.CENTER);
+		add(jautajumuLauks,BorderLayout.NORTH);
+		atbilzuLogs = new JPanel();
+		atbilzuLogs.setLayout(new GridLayout(4, 1, 5, 5));
+		atbilzuPogas = new JRadioButton[4];		
+		poguGrupa = new ButtonGroup();
+		parbauditPoga = new JButton("Pārbaudīt atbildi");
+		parbauditPoga.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		add(atbilzuLogs,BorderLayout.CENTER);
+		add(parbauditPoga,BorderLayout.SOUTH);
 		setVisible(true);
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new ElektroniskaisTests();
 	}
 
