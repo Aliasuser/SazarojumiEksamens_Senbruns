f8dcaf212434ae9234102be118cfe4eb70491d37
AliasXIX <arnis.senbruns@sk.lvt.lv>
2025-06-07 13:19:04 +0300

    Izveidots sākuma GUI priekš elektroniskā testa.

--- src/ElektroniskaisTests.java
+++ src/ElektroniskaisTests.java
@@ -1,9 +1,22 @@
+import javax.swing.JFrame;
 
-public class ElektroniskaisTests {
+public class ElektroniskaisTests extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public ElektroniskaisTests() {
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setTitle("Java Elektroniskais Tests");
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new ElektroniskaisTests();
 	}
 
 }
